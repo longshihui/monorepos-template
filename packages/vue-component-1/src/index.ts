083a5b8ab3f1132e09ff68be9030b09586a72bb4
longshihui <391424924@qq.com>
2022-06-18 22:25:22 +0800

    fix: 修复代码之前的报错

--- packages/vue-component-1/src/index.ts
+++ packages/vue-component-1/src/index.ts
@@ -1,3 +1,6 @@
 import Example1 from "./Example1.vue";
+import { sayHello } from "@example/shared";
+
+sayHello();
 
 export default Example1;
--- packages/vue-component-2/src/index.ts
+++ packages/vue-component-2/src/index.ts
@@ -1,6 +1,8 @@
 import Example2 from "./Example2.vue";
-import Example1 from "@monorepos-example/example1";
-import {} from "@monorepos-example/shared";
+import Example1 from "@example/vue-component-1";
+import { sayHello } from "@example/shared";
+
+sayHello();
 
 export { Example1 };
 
